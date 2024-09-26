c52c7a628f875d22cc6394b0a529ffe6f7798e3a
Nicholas Davison <nick.a.davison@gmail.com>
2024-09-26 14:37:32 -0500

    created route group for auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
                 <Button 
                     className="mt-2 flex justify-center"
                     
-                >Login with Github</Button>
+                >Log In</Button>
             </div>
         </main>
     )
